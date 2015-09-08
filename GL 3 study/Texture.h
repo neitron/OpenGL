fa2fd78616f1ce940a484c388f869ef254009c65
Neitron <kirill.zubenko@mal.ru>
2015-09-09 00:35:55 +0300

    Texture

--- GL 3 study/Texture.h
+++ GL 3 study/Texture.h
@@ -4,12 +4,17 @@
 #pragma once
 #include <GL\glut.h>
 #include <string>
+#include <ImageMagick\Magick++.h>
 
 class Texture
 {
 public:
-    //Texture(GLenum TextureTarget, const std::string& FileName);
- 
+  
+    Texture(GLenum TextureTarget, const std::string& FileName)
+    {
+      Magick::Image* i = 0;
+    }
+    
     //bool Load();
  
     //void Bind(GLenum TextureUnit);
